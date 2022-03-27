04b62419f92f10ab90f0b1dfca35a5fbcc5c2c13
Jumana <jahanashaji2020@gmail.com>
2022-03-27 18:18:15 +0530

    Created employee table to add the employee details

--- CRUDAPP/models.py
+++ CRUDAPP/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Employeedb(models.Model):
+    employee_name = models.CharField(max_length=100)
+    employee_email = models.EmailField()
+    employee_age = models.IntegerField()
+    employee_image = models.ImageField(upload_to='employee_image')
+    
+    def __str__(self):
+        return self.employee_name
